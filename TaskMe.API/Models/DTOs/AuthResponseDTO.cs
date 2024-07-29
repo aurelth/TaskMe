e7d1c076dd087aae1eafcbb7aa8bff8670a783f7
Aurel Lossou <aurelth@icloud.com>
2024-07-29 11:27:42 -0300

    DTOs criados

---
+++ TaskMe.API/Models/DTOs/AuthResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskMe.API.Models.DTOs
+{
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ TaskMe.API/Models/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskMe.API.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
